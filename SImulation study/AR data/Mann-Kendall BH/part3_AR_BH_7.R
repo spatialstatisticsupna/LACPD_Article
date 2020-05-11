e75cc807a1f63bc267f789db326de610a6ebd478
Mehdi Moradi <Moradi@uji.es>
2020-05-11 22:40:40 +0200

    simulation study running codes

---
+++ SImulation study/AR data/Mann-Kendall BH/part3_AR_BH_7.R
@@ -0,0 +1,40 @@
+library(Scp)
+library(parallel)
+set.seed(1234)
+x <- replicate(500,arima.sim(168,model = list(ar=0.5)),simplify = FALSE)
+
+R3 <- mclapply(X=1:length(x), function(i){
+  if(i<length(x)) {
+    cat(paste(i),",")
+    flush.console()
+  } else {
+    cat(paste(i),"\n")
+    flush.console()
+  }  
+  y <- c(x[[i]][1:120],x[[i]][121:168]+3)
+  
+  out1 <- Re_mk(y,m=100,k=c(2:30),adjust = TRUE,method="BH",
+                blow = 0.1,history = TRUE)
+  
+  return(out1)
+  
+})
+
+save.image("part3_AR_BH_7.RData")
+
+
+cps <- matrix(nrow = nrow(attr(R3[[1]],"hist")),ncol=length(R3))
+for (i in 1:length(R3)) {
+  cps[,i] <- attr(R3[[i]],"hist")[,1]
+}
+rownames(cps) <- rownames(attr(R3[[1]],"hist"))
+MAE <- rowMeans(abs(cps-120))
+MAE[which.min(MAE)]
+
+pss <- matrix(nrow = nrow(attr(R3[[1]],"hist")),ncol=length(R3))
+for (i in 1:length(R3)) {
+  pss[,i] <- attr(R3[[i]],"hist")[,4]
+}
+rownames(pss) <- rownames(attr(R3[[1]],"hist"))
+power <- rowMeans(pss<0.05)
+power[which.max(power)]
---
+++ SImulation study/Normal data/Mann-Kendall BH/part1_normal_BH_7.R
@@ -0,0 +1,40 @@
+library(Scp)
+library(parallel)
+set.seed(1234)
+x <- replicate(500,rnorm(168),simplify = FALSE)
+
+R1 <- mclapply(X=1:length(x), function(i){
+  if(i<length(x)) {
+    cat(paste(i),",")
+    flush.console()
+  } else {
+    cat(paste(i),"\n")
+    flush.console()
+  }  
+  y <- c(x[[i]][1:40],x[[i]][41:168]+1.5)
+  
+  out1 <- Re_mk(y,m=100,k=c(2:30),adjust = TRUE,method="BH",
+                   blow = 0.1,history = TRUE)
+  
+  return(out1)
+  
+})
+
+save.image("part1_normal_BH_7.RData")
+
+
+cps <- matrix(nrow = nrow(attr(R1[[1]],"hist")),ncol=length(R1))
+  for (i in 1:length(R1)) {
+    cps[,i] <- attr(R1[[i]],"hist")[,1]
+  }
+rownames(cps) <- rownames(attr(R1[[1]],"hist"))
+MAE <- rowMeans(abs(cps-40))
+MAE[which.min(MAE)]
+
+pss <- matrix(nrow = nrow(attr(R1[[1]],"hist")),ncol=length(R1))
+for (i in 1:length(R1)) {
+  pss[,i] <- attr(R1[[i]],"hist")[,4]
+}
+rownames(pss) <- rownames(attr(R1[[1]],"hist"))
+power <- rowMeans(pss<0.05)
+power[which.max(power)]
