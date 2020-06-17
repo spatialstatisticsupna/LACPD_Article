72ff923d410481a17feb32556781228577c1575e
Mehdi Moradi <Moradi@uji.es>
2020-06-17 11:23:41 +0200

    AR Mann-Kendall BY

--- SImulation study/AR data/Mann-Kendall BY/false_AR_BY.R
+++ SImulation study/AR data/Mann-Kendall BY/false_AR_BY.R
@@ -1,4 +1,4 @@
-library(Scp)
+library(LACPD)
 library(parallel)
 set.seed(1234)
 x <- replicate(500,arima.sim(168,model = list(ar=0.5)),simplify = FALSE)
@@ -13,19 +13,19 @@ R <- mclapply(X=1:length(x), function(i){
   }  
   y <- x[[i]]
   
-  out1 <- Re_mk(y,m=100,k=c(2:30),adjust = TRUE,method="BY",
+  out1 <- lacpd_mk(y,m=100,k=c(2:30),adjust = TRUE,method="BY",
                 blow = 0.1,history = TRUE)
   
   return(out1)
   
 })
 
-save.image("false_AR_BY_7.RData")
+save.image("false_AR_BY.RData")
 
-pss <- matrix(nrow = nrow(attr(R[[1]],"hist")),ncol=length(R))
-for (i in 1:length(R)) {
-  pss[,i] <- attr(R[[i]],"hist")[,4]
-}
-rownames(pss) <- rownames(attr(R[[1]],"hist"))
-FP <- rowMeans(pss<0.05)
-FP[which.min(FP)]
+# pss <- matrix(nrow = nrow(attr(R[[1]],"hist")),ncol=length(R))
+# for (i in 1:length(R)) {
+#   pss[,i] <- attr(R[[i]],"hist")[,4]
+# }
+# rownames(pss) <- rownames(attr(R[[1]],"hist"))
+# FP <- rowMeans(pss<0.05)
+# FP[which.min(FP)]
